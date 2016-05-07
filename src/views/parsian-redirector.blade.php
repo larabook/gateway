1a456550f10b3d1cff98da3b2094907147eb00d7
Hamed <iraitc@gmail.com>
2016-05-07 22:40:49 +0430

    Delete parsian-redirector.blade.php

--- src/views/parsian-redirector.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{!! $url !!}" method="post" id="parsian_gateway"></form>
-
-<script type="text/javascript">
-	var f=document.getElementById('parsian_gateway');
-	f.submit();
-</script>
