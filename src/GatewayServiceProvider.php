29fbe6bfd5eb8d4594a99dd90bd2ae40cb28b856
Hamed <iraitc@gmail.com>
2016-05-17 20:05:13 +0430

    Update GatewayServiceProvider.php
    اگر 
    defer = true
    باشه دستورات پابلیش دیگه اجرا نمیشن !

--- src/GatewayServiceProvider.php
+++ src/GatewayServiceProvider.php
@@ -11,7 +11,7 @@ class GatewayServiceProvider extends ServiceProvider
 	 *
 	 * @var bool
 	 */
-	protected $defer = true;
+	protected $defer = false;
 
 	/**
 	 * Bootstrap the application services.
