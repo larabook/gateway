419439a3c5cb81fcba6dfba1f133bfc1f19fd4c5
mohammad hassan bolivand <mohammadhassanbolivand@gmail.com>
2020-02-13 12:27:38 +0330

    add doc for better auto complete

--- src/Gateway.php
+++ src/Gateway.php
@@ -6,6 +6,20 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Larabookir\Gateway\GatewayResolver
+ * @method  static GatewayResolver make($port)
+ * @method  static GatewayResolver verify()
+ * @method  static GatewayResolver mellat()
+ * @method  static GatewayResolver sadad()
+ * @method  static GatewayResolver zarinpal()
+ * @method  static GatewayResolver payline()
+ * @method  static GatewayResolver jahanpay()
+ * @method  static GatewayResolver parsian()
+ * @method  static GatewayResolver pasargad()
+ * @method  static GatewayResolver saman()
+ * @method  static GatewayResolver asanpardakht()
+ * @method  static GatewayResolver paypal()
+ * @method  static GatewayResolver payir()
+ * @method  static GatewayResolver irankish()
  */
 class Gateway extends Facade
 {
